f5d5634ec2f86aa385e8500406852281837e08a1
77u4 <github@jh0.eu>
2015-01-02 05:12:11 +0100

    Tried various things out with pushover

--- lib/urlwatch/pushover.py
+++ lib/urlwatch/pushover.py
@@ -1,5 +1,9 @@
 from pushover import Client
 
-def send(user_key, api_token, message, title)
+# SETUP
+	user_key  = ''
+	api_token = ''
+
+def send(title, message)
 	client = Client(user_key, api_token)
 	client.send_message(message, title)
